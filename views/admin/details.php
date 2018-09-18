ae0ae17460d1f9bd358dd9792403f04926a8dea3
cheb4 <cheb@gmail.com>
2018-09-18 20:22:46 +0300

    update

--- views/admin/details.php
+++ views/admin/details.php
@@ -3,3 +3,11 @@
 echo "<hr>cia bus details<hr>";
 
 echo $oneItem['name'];
+
+echo "id ".$oneItem['id']."<br>";
+echo "vardas ".$oneItem['name']."<br>";
+echo "".$oneItem['surname']."<br>";
+echo "".$oneItem['city']."<br>";
+echo "".$oneItem['orderNum']."<br>";
+echo "".$oneItem['phone']."<br>";
+echo "".$oneItem['comment']."<br>";
