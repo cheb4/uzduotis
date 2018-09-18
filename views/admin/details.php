434443761af681ea0687e6159efe105666ec8141
cheb4 <cheb@gmail.com>
2018-09-18 20:28:45 +0300

    update

--- views/admin/details.php
+++ views/admin/details.php
@@ -6,8 +6,8 @@ echo $oneItem['name'];
 
 echo "id ".$oneItem['id']."<br>";
 echo "vardas ".$oneItem['name']."<br>";
-echo "".$oneItem['surname']."<br>";
-echo "".$oneItem['city']."<br>";
-echo "".$oneItem['orderNum']."<br>";
-echo "".$oneItem['phone']."<br>";
-echo "".$oneItem['comment']."<br>";
+echo "pavarde ".$oneItem['surname']."<br>";
+echo "Miestas ".$oneItem['city']."<br>";
+echo "uzsakymo skaicius".$oneItem['orderNum']."<br>";
+echo "telefono numeris".$oneItem['phone']."<br>";
+echo "<br> komentaras".$oneItem['comment']."<br>";
