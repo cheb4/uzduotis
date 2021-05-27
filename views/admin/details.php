2494cf53125ed61891731dfc016c3de70e5c8e2c
cheb4 <a.splitas@gmail.com>
2021-05-28 01:45:48 +0300

    details

--- views/admin/details.php
+++ views/admin/details.php
@@ -12,41 +12,43 @@
 
 <body>
   <!--change when uploading to server  -->
-  <div id="main-container">
-    <div id="header">
-      <ul class="nav">
-        <li><a href="/order-form">Užsakymo forma</a></li>
-        <li><a href=/admin/index.php?page=1>Užsakymų sąrašas</a> </li> </ul> </div> </div> </body> </html> <table>
-            <tr>
-              <th>id</th>
-              <td><?php echo $oneItem['id']; ?></td>
-            </tr>
-            <tr>
-              <th>vardas</th>
-              <td><?php echo $oneItem['name']; ?></td>
-            </tr>
-            <tr>
-              <th>pavarde</th>
-              <td><?php echo $oneItem['surname']; ?></td>
-            </tr>
-            <tr>
-              <th>Miestas</th>
-              <td><?php echo $oneItem['city']; ?></td>
-            </tr>
-            <tr>
-              <th>uzsakymo vieta</th>
-              <td><?php echo $oneItem['orderNum']; ?></td>
-            </tr>
-            <tr>
-              <th>telefono numeris</th>
-              <td><?php echo $oneItem['phone']; ?></td>
-            </tr>
-            <tr>
-              <th>komentaras</th>
-              <td><?php echo $oneItem['comment']; ?></td>
-            </tr>
-            </tr>
-            <tr>
+  <div class="details-container">
+    <div class="details">
+      <div id="main-container">
+        <div id="header">
+          <ul class="nav">
+            <li><a href="/order-form">Užsakymo forma</a></li>
+            <li><a href=/admin/index.php?page=1>Užsakymų sąrašas</a> </li> </ul> </div> </div> </body> </html> <table>
+                <tr>
+                  <th>id</th>
+                  <td><?php echo $oneItem['id']; ?></td>
+                </tr>
+                <tr>
+                  <th>vardas</th>
+                  <td><?php echo $oneItem['name']; ?></td>
+                </tr>
+                <tr>
+                  <th>pavarde</th>
+                  <td><?php echo $oneItem['surname']; ?></td>
+                </tr>
+                <tr>
+                  <th>Miestas</th>
+                  <td><?php echo $oneItem['city']; ?></td>
+                </tr>
+                <tr>
+                  <th>uzsakymo vieta</th>
+                  <td><?php echo $oneItem['orderNum']; ?></td>
+                </tr>
+                <tr>
+                  <th>telefono numeris</th>
+                  <td><?php echo $oneItem['phone']; ?></td>
+                </tr>
+                <tr>
+                  <th>komentaras</th>
+                  <td><?php echo $oneItem['comment']; ?></td>
+                </tr>
+                </tr>
+                <!-- <tr>
               <th>trinti</th>
               <td>
                 <form method="POST" action=''>
@@ -54,10 +56,10 @@
 
                 </form>
               </td>
-            </tr>
+            </tr> -->
 
-            </table>
-            <?php
+                </table>
+                <?php
 
 
             function getConnection()
@@ -94,7 +96,13 @@
 
 
             ?>
+        </div>
+      </div>
+    </div>
+    <div class="details-map">
+        <div class="mapouter"><div class="gmap_canvas"><iframe width="100%" height="100%" id="gmap_canvas" src="https://maps.google.com/maps?q=Joni%C5%A1kis,%20%C5%BEemes%20ukio%20mokykla&t=k&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://123movies-a.com"></a><br><style>.mapouter{position:relative;text-align:right;height:500px;width:500px;}</style><a href="https://www.embedgooglemap.net">custom google map for website</a><style>.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:500px;}</style></div></div>
     </div>
 </body>
+
 
 </html>
