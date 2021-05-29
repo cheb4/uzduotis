938cb703864e51c6bd28a866e157b0cb2e2089aa
cheb4 <a.splitas@gmail.com>
2021-05-29 20:47:09 +0300

    front page

--- views/main/index.php
+++ views/main/index.php
@@ -11,7 +11,7 @@
 </head>
 
 <body>
-    <div id="start">
+    <!-- <div id="start">
 
 
         <div class="split left">
@@ -37,70 +37,61 @@
 
             </div>
         </div>
-    </div>
+    </div> -->
 
 
 
     <div id="main-container-order">
         <div class="forma">
-        <div id="header">
-            <ul class="nav">
+            <div id="header">
+                <ul class="nav">
+                    <!-- navigacija -->
+                    <li><a href="/order-form">Užsakymo forma</a></li>
+                    <li><a href="/admin/index.php?page=1">Užsakymų sąrašas</a></li>
+                </ul>
                 <!-- navigacija -->
-                <li><a href="/order-form">Užsakymo forma</a></li>
-                <li><a href="/admin/index.php?page=1">Užsakymų sąrašas</a></li>
-            </ul>
-            <!-- navigacija -->
+            </div>
+            <form id="order-form" action="/insert" method="POST">
+                <div class="form-field">
+                    <label>Vardas</label>
+                    <input type="text" name="name">
+                </div>
+                <div class="form-field">
+                    <label>Pavarde</label>
+                    <input type="text" name="surname">
+                </div>
+                <div class="form-field">
+                    <label>Miestas</label>
+                    <input type="text" name="town">
+                </div>
+                <div class="form-field">
+                    <label>Vieta</label>
+                    <select name="orderSize">
+                        <option value="1">Nr 1</option>
+                        <option value="2">Nr 2</option>
+                        <option value="3">Nr 3</option>
+                        <option value="4">Nr 4</option>
+                        <option value="5">Nr 5</option>
+                        <option value="6">Nr 6</option>
+                    </select>
+                </div>
+                <div class="form-field">
+                    <label>Telefonas</label>
+                    <input type="text" name="phoneNumber">
+                </div>
+                <div class="form-field">
+                    <label>Komentaras</label>
+                    <textarea name="comment"></textarea>
+                </div>
+                <button type="submit">Pateikti</button>
+            </form>
         </div>
-        <form id="order-form" action="/insert" method="POST">
-            <div class="form-field">
-                <label>Vardas</label>
-                <input type="text" name="name">
-            </div>
-            <div class="form-field">
-                <label>Pavarde</label>
-                <input type="text" name="surname">
-            </div>
-            <div class="form-field">
-                <label>Miestas</label>
-                <input type="text" name="town">
-            </div>
-            <div class="form-field">
-                <label>Vieta</label>
-                <select name="orderSize">
-                    <option value="1">Nr 1</option> 
-                    <option value="2">Nr 2</option> 
-                    <option value="3">Nr 3</option> 
-                    <option value="4">Nr 4</option> 
-                    <option value="5">Nr 5</option> 
-                    <option value="6">Nr 6</option> 
-                </select>
-            </div>
-            <div class="form-field">
-                <label>Telefonas</label>
-                <input type="text" name="phoneNumber">
-            </div>
-            <div class="form-field">
-                <label>Komentaras</label>
-                <textarea name="comment"></textarea>
-            </div>
-            <button type="submit">Pateikti</button>
-        </form>
-        </div>
-        <div class="map">
-        <div class="mapouter"><div class="gmap_canvas"><iframe width="500" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=Joni%C5%A1kis,%20%C5%BEemes%20ukio%20mokykla&t=k&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://123movies-a.com"></a><br><style>.mapouter{position:relative;text-align:right;height:500px;width:500px;}</style><a href="https://www.embedgooglemap.net">custom google map for website</a><style>.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:500px;}</style></div></div>
-</div>
+        <!-- <div class="map"> -->
+        <!-- <div class="mapouter"><div class="gmap_canvas"><iframe width="500" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=Joni%C5%A1kis,%20%C5%BEemes%20ukio%20mokykla&t=k&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://123movies-a.com"></a><br><style>.mapouter{position:relative;text-align:right;height:500px;width:500px;}</style><a href="https://www.embedgooglemap.net">custom google map for website</a><style>.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:500px;}</style></div></div> -->
     </div>
+    <!-- </div> -->
 
 </body>
-<script>
-    document.querySelector("#start > div.split.left").addEventListener("click", function (e) {
 
-        document.getElementById("start").remove()
-    });
-    document.querySelector("#start > div.split.right").addEventListener("click", function (e) {
-        window.open("https://desolate-reaches-61237.herokuapp.com/admin/index.php?page=1", "_self");
-        // document.getElementById("start").remove()
-    });
-</script>
 
 </html>
--- views/start/index.php
+++ views/start/index.php
@@ -1,12 +1,61 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-   opa 
-</body>
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Order Form</title>
+        <link rel="stylesheet" href="./reset.css">
+        <link rel="stylesheet" href="./main.css">
+
+    <body>
+        <div class="pagrindinis">
+            <div class="register">
+                <h1 class="mainDesc">Užsakyti vietą</h1>
+
+
+            </div>
+
+            <div class="map">
+                <div class="mapouter">
+                    <div class="gmap_canvas"><iframe width="500" height="500" id="gmap_canvas"
+                            src="https://maps.google.com/maps?q=Joni%C5%A1kis,%20%C5%BEemes%20ukio%20mokykla&t=k&z=17&ie=UTF8&iwloc=&output=embed"
+                            frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a
+                            href="https://123movies-a.com"></a><br>
+                        <style>
+                            .mapouter {
+                                /* position: relative; */
+                                /* text-align: right; */
+                                height: 900px;
+                                width: 600px;
+                            }
+                        </style><a href="https://www.embedgooglemap.net">custom google map for website</a>
+                        <style>
+                            .gmap_canvas {
+                                overflow: hidden;
+                                background: none !important;
+                                height: 900px;
+                                width: 600px;
+                            }
+                        </style>
+                    </div>
+                </div>
+            </div>
+
+            <div class="list">
+                <h1 class="mainDesc">Užsakytų vietų sąrašas</h1>
+            </div>
+        </div>
+    </body>
+    <script>
+        document.querySelector(".list").addEventListener("click", function (e) {
+            window.open("https://desolate-reaches-61237.herokuapp.com/admin/index.php?page=1", "_self");
+        });
+document.querySelector(".register").addEventListener("click", function (e) {
+            window.open("https://desolate-reaches-61237.herokuapp.com/register", "_self");
+        });
+    </script>
+
 </html>
