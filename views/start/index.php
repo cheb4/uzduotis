e844fd342fc57f4afda0fb64bb29f037ce7ea490
cheb4 <a.splitas@gmail.com>
2021-05-31 23:37:26 +0300

    test

--- views/start/index.php
+++ views/start/index.php
@@ -19,7 +19,7 @@
             </div>
 
             <div class="map">
-                <div class="mapouter">
+                <!-- <div class="mapouter">
                     <div class="gmap_canvas"><iframe width="500" height="500" id="gmap_canvas"
                             src="https://maps.google.com/maps?q=Joni%C5%A1kis,%20%C5%BEemes%20ukio%20mokykla&t=k&z=17&ie=UTF8&iwloc=&output=embed"
                             frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a
@@ -41,7 +41,7 @@
                             }
                         </style>
                     </div>
-                </div>
+                </div> -->
             </div>
 
             <div class="list">
@@ -50,6 +50,23 @@
         </div>
     </body>
     <script>
+// pradzia
+function initMap() {
+        const myLatLng = { lat: -25.363, lng: 131.044 };
+        const map = new google.maps.Map(document.getElementById("map"), {
+          zoom: 4,
+          center: myLatLng,
+        });
+        new google.maps.Marker({
+          position: myLatLng,
+          map,
+          title: "Hello World!",
+        });
+      }
+
+
+// pabaiga
+
         document.querySelector(".list").addEventListener("click", function (e) {
             window.open("https://desolate-reaches-61237.herokuapp.com/admin/index.php?page=1", "_self");
         });
@@ -57,5 +74,9 @@ document.querySelector(".register").addEventListener("click", function (e) {
             window.open("https://desolate-reaches-61237.herokuapp.com/register", "_self");
         });
     </script>
+    <scrip
+          src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDPSIXnA8K7gndiwijS1Iy8q9c3L3HLfHo&callback=initMap&libraries=&v=weekly"
+      async
+    ></scrip>
 
 </html>
