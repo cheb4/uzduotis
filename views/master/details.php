4c7bd284406754bf30210f3206296a5b3f82588b
cheb4 <a.splitas@gmail.com>
2021-06-02 01:04:44 +0300

    details master update

--- views/master/details.php
+++ views/master/details.php
@@ -4,7 +4,9 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <title>Order Form</title>
+  <!-- <title>Details</title> -->
+   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDPSIXnA8K7gndiwijS1Iy8q9c3L3HLfHo&callback=initMap&libraries=&v=weekly"
+          type="text/javascript"></script>
   <link rel="stylesheet" href="https://desolate-reaches-61237.herokuapp.com/reset.css">
   <link rel="stylesheet" href="https://desolate-reaches-61237.herokuapp.com/main.css">
 </head>
