f79b5493828c3a400c1c29b47f479059c4115db1
cheb4 <a.splitas@gmail.com>
2021-06-02 00:27:06 +0300

    admin map change

--- views/admin/index.php
+++ views/admin/index.php
@@ -5,6 +5,8 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <title>Order List</title>
+      <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDPSIXnA8K7gndiwijS1Iy8q9c3L3HLfHo&callback=initMap&libraries=&v=weekly"
+          type="text/javascript"></script>
   <link rel="stylesheet" href="https://desolate-reaches-61237.herokuapp.com/reset.css">
   <link rel="stylesheet" href="https://desolate-reaches-61237.herokuapp.com/main.css">
 
@@ -13,33 +15,33 @@
 <body>
   <div id="main-container">
     <div class="admin-container">
-    <div class="table-container">
+      <div class="table-container">
 
-      <div id="header">
-        <ul class="nav">
-          <li><a href="/order-form">Užsakymo forma</a></li>
-          <!-- <li><a href="/product-list">Užsakymų sąrašas</a></li> -->
-        </ul>
-      </div>
+        <div id="header">
+          <ul class="nav">
+            <li><a href="/order-form">Užsakymo forma</a></li>
+            <!-- <li><a href="/product-list">Užsakymų sąrašas</a></li> -->
+          </ul>
+        </div>
 
 
-      <table class="tg">
-        <tr>
-          <th class="tg-0lax">id <a
-              href="https://desolate-reaches-61237.herokuapp.com/admin/id_asc/index.php?page=1">↑</a> <a
-              href="https://desolate-reaches-61237.herokuapp.com/admin/id_desc/index.php?page=1">↓</a> </th>
-          <th class="tg-0lax">vardas <a
-              href="https://desolate-reaches-61237.herokuapp.com/admin/name_asc/index.php?page=1">↑</a> <a
-              href="https://desolate-reaches-61237.herokuapp.com/admin/name_desc/index.php?page=1">↓</a> </th>
-          <!-- <th class="tg-0lax">pavarde <a href="https://desolate-reaches-61237.herokuapp.com/admin/surname_asc/index.php?page=1">↑</a> <a href="https://desolate-reaches-61237.herokuapp.com/admin/surname_desc/index.php?page=1">↓</a> </th> -->
-          <!-- <th class="tg-0lax">miestas <a href="https://desolate-reaches-61237.herokuapp.com/admin/city_asc/index.php?page=1">↑</a> <a href="https://desolate-reaches-61237.herokuapp.com/admin/city_desc/index.php?page=1">↓</a> </th> -->
-          <th class="tg-0lax">užsakyta vieta <a
-              href="https://desolate-reaches-61237.herokuapp.com/admin/orderNum_asc/index.php?page=1">↑</a> <a
-              href="https://desolate-reaches-61237.herokuapp.com/admin/orderNum_desc/index.php?page=1">↓</a> </th>
-          <!-- <th class="tg-0lax">telefono numeris</th> -->
-          <!-- <th class="tg-baqh">komentaras</th> -->
-        </tr>
-        <?php foreach ($itemList as $item) {
+        <table class="tg">
+          <tr>
+            <th class="tg-0lax">id <a
+                href="https://desolate-reaches-61237.herokuapp.com/admin/id_asc/index.php?page=1">↑</a> <a
+                href="https://desolate-reaches-61237.herokuapp.com/admin/id_desc/index.php?page=1">↓</a> </th>
+            <th class="tg-0lax">vardas <a
+                href="https://desolate-reaches-61237.herokuapp.com/admin/name_asc/index.php?page=1">↑</a> <a
+                href="https://desolate-reaches-61237.herokuapp.com/admin/name_desc/index.php?page=1">↓</a> </th>
+            <!-- <th class="tg-0lax">pavarde <a href="https://desolate-reaches-61237.herokuapp.com/admin/surname_asc/index.php?page=1">↑</a> <a href="https://desolate-reaches-61237.herokuapp.com/admin/surname_desc/index.php?page=1">↓</a> </th> -->
+            <!-- <th class="tg-0lax">miestas <a href="https://desolate-reaches-61237.herokuapp.com/admin/city_asc/index.php?page=1">↑</a> <a href="https://desolate-reaches-61237.herokuapp.com/admin/city_desc/index.php?page=1">↓</a> </th> -->
+            <th class="tg-0lax">užsakyta vieta <a
+                href="https://desolate-reaches-61237.herokuapp.com/admin/orderNum_asc/index.php?page=1">↑</a> <a
+                href="https://desolate-reaches-61237.herokuapp.com/admin/orderNum_desc/index.php?page=1">↓</a> </th>
+            <!-- <th class="tg-0lax">telefono numeris</th> -->
+            <!-- <th class="tg-baqh">komentaras</th> -->
+          </tr>
+          <?php foreach ($itemList as $item) {
 
         echo "<tr>";
         echo "<td class='tg-0lax'>" . '<a href="https://desolate-reaches-61237.herokuapp.com/admin-' . $item['id'] . '">' . $item['id'] . " </td>";
@@ -52,9 +54,9 @@
         echo "</tr>";
       }
       ?>
-      </table>
+        </table>
 
-      <?
+        <?
 
 for ($page=1;$page<=$pageNum;$page++) {
     echo '<a href="index.php?page=' . $page . '">' . $page . '</a> ';
@@ -62,12 +64,76 @@ for ($page=1;$page<=$pageNum;$page++) {
 ?>
 
 
+      </div>
+      <div class="map-table">
+        <div id="locationReg" style="width: 700px; height: 700px;"></div>
+        <!-- <div class="mapouter">
+          <div class="gmap_canvas"><iframe width="100%" height="100%" id="gmap_canvas"
+              src="https://maps.google.com/maps?q=Joni%C5%A1kis,%20%C5%BEemes%20ukio%20mokykla&t=k&z=17&ie=UTF8&iwloc=&output=embed"
+              frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a
+              href="https://123movies-a.com"></a><br>
+            <style>
+              .mapouter {
+                position: relative;
+                text-align: right;
+                height: 500px;
+                width: 500px;
+              }
+            </style><a href="https://www.embedgooglemap.net">custom google map for website</a>
+            <style>
+              .gmap_canvas {
+                overflow: hidden;
+                background: none !important;
+                height: 500px;
+                width: 500px;
+              }
+            </style>
+          </div>
+        </div> -->
+      </div>
     </div>
-    <div class="map-table">
-        <div class="mapouter"><div class="gmap_canvas"><iframe width="100%" height="100%" id="gmap_canvas" src="https://maps.google.com/maps?q=Joni%C5%A1kis,%20%C5%BEemes%20ukio%20mokykla&t=k&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://123movies-a.com"></a><br><style>.mapouter{position:relative;text-align:right;height:500px;width:500px;}</style><a href="https://www.embedgooglemap.net">custom google map for website</a><style>.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:500px;}</style></div></div>
-</div>
-</div>
   </div>
 </body>
+ <script>
+// prdzia
+var locations = [
+      ['pirmas', 56.241929104940375, 23.634707957688992],
+      ['antras', 56.241718628143777, 23.634707957688992],
+      ['trecias', 56.2415081513472, 23.634707957688992],
+      ['ketvirtas',56.2412976745506, 23.634707957688992 ],
+      ['penktas', 56.241087197754, 23.634707957688992],
+      ['sestas',56.241929104940375, 23.635713017406772 ],
+      ['septintas',56.241718628143777, 23.635713017406772 ],
+      ['astuntas', 56.2415081513472, 23.635713017406772],
+      ['devintas',56.2412976745506, 23.635713017406772 ],
+      ['desimtas',56.241087197754, 23.635713017406772 ],
+    ];
 
+    var map = new google.maps.Map(document.getElementById('locationReg'), {
+      zoom: 17.5,
+      center: new google.maps.LatLng(56.24168095734993, 23.634612266633244),
+      mapTypeId: 'satellite'
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) {  
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map
+      });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+    }
+
+// pabaiga
+
+    </script>
 </html>
