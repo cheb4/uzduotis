dfa36a598176447c7969d3cc53dd46bbaecf0ede
cheb4 <a.splitas@gmail.com>
2021-06-01 11:02:03 +0300

    db change

--- config/db_params.php
+++ config/db_params.php
@@ -1,12 +1,14 @@
 <?php
 // $clearDbUrl = parse_url(getenv("mysql://b32e344c938404:61db3fa4@us-cdbr-iron-east-01.cleardb.net/heroku_7fe484b29314a31?reconnect=true"));
 
-// return array(
-//             'host' => $clearDbUrl['host'],
-//             'dbname' => substr($clearDbUrl['path'],1),
-//             'user' => $clearDbUrl['user'],
-//             'password' => $clearDbUrl['pass'],
-// );
+$clearDbUrl = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+return array(
+            'host' => $clearDbUrl['host'],
+            'dbname' => substr($clearDbUrl['path'],1),
+            'user' => $clearDbUrl['user'],
+            'password' => $clearDbUrl['pass'],
+);
 
 // $clearDbUrl = parse_url(getenv("CLEARDB_DATABASE_URL"));
 // $clearDbHost = $clearDbUrl['host'];
@@ -21,9 +23,9 @@
 //  [path] => /heroku_7fe484b29314a31 
 //  [query] => reconnect=true )
 
-return array(
-            'host' => 'us-cdbr-iron-east-01.cleardb.net ',
-            'dbname' => 'heroku_7fe484b29314a31',
-            'user' => 'b32e344c938404',
-            'password' => '61db3fa4',
-);
+// return array(
+//             'host' => 'us-cdbr-iron-east-01.cleardb.net ',
+//             'dbname' => 'heroku_7fe484b29314a31',
+//             'user' => 'b32e344c938404',
+//             'password' => '61db3fa4',
+// );
