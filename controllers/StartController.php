0243233564f44c39e847dc9e5f90d90ec2d1141c
cheb4 <a.splitas@gmail.com>
2021-06-02 23:02:30 +0300

    sta

--- controllers/StartController.php
+++ controllers/StartController.php
@@ -1,6 +1,7 @@
 <?php
 
 // include_once ROOT. '/models/Start.php';
+include_once ROOT. '/models/Admin.php';
 
 // 
 
@@ -14,14 +15,29 @@ class StartController
         // require_once(ROOT . '/views/start/index.php');
     // }
 
+    // public function actionDisplay()
+    // {
+    //     // $this->displayEverything();
+    //     // $allResult = Start::getAllData();
+    //     // require_once(ROOT . '/views/start/index.php');
+    //     return true;
+    // }
+
+    public function displayEverything($sql)
+    {
+        $allResult = Admin::getAllData();
+        $itemList = Admin::getPagination($sql);
+        $pageNum = Admin::getPaginationNum();
+        require_once(ROOT . '/views/start/index.php');
+        // require_once(ROOT . '/views/admin/index.php');
+    }
+// action display kada gauna nuoruoda uzkrauna viska 
+
     public function actionDisplay()
     {
-        // $this->displayEverything();
-        // $allResult = Start::getAllData();
-        require_once(ROOT . '/views/start/index.php');
+        $this->displayEverything("SELECT * FROM customers");
         return true;
     }
-
 
 
 
